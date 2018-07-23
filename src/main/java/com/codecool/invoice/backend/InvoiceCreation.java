a7480b315aef1063c897a06a34c5747ac7e92719
johnny-sack1 <jonymaka@gmail.com>
2018-07-23 15:01:56 +0200

    Create HTTP handlers

---
+++ src/main/java/com/codecool/invoice/backend/InvoiceCreation.java
@@ -0,0 +1,43 @@
+package com.codecool.invoice.backend;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InvoiceCreation implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String line;
+        String response = "";
+
+        try {
+            File html = new File("/home/jonatan/codecool/module3/TW2/invoice/src/main/java/com/codecool/invoice/frontend/create.html");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(html)));
+
+            while ((line = bufferedReader.readLine()) != null) {
+                response += line;
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sendResponse(exchange, response);
+    }
+
+    public void sendResponse(HttpExchange exchange, String response) {
+        byte[] bytes = response.getBytes();
+        try {
+            exchange.sendResponseHeaders(200, bytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
